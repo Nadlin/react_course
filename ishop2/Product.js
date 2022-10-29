5c9e3446222aaf04985cb2f3e1dc2fb10f19826d
nadezhda-linnik <bolik2@list.ru>
2022-10-29 03:17:52 +0300

    ishop2

---
+++ ishop2/Product.js
@@ -0,0 +1,44 @@
+﻿var Product = React.createClass({
+
+    displayName: 'Product',
+
+    propTypes: {
+        name: React.PropTypes.string.isRequired,
+        code: React.PropTypes.number.isRequired,
+        qty: React.PropTypes.number.isRequired,
+        price: React.PropTypes.number.isRequired,
+        url: React.PropTypes.string.isRequired,
+        cbSelectedProduct: React.PropTypes.func.isRequired,
+        selectedProduct: React.PropTypes.number,
+        cbDeleteProduct: React.PropTypes.func.isRequired,
+        deletedProduct: React.PropTypes.number
+    },
+
+    selectProduct: function (eo) {
+        if (eo.target.value != 'Delete') {
+            this.props.cbSelectedProduct(this.props.code);
+        }
+    },
+
+    deleteProduct: function (eo) {
+        var answer = window.confirm("Do you really want to delete the product?");
+        if (answer) {
+            this.props.cbDeleteProduct(this.props.code);
+        }
+    },
+
+    render: function() {
+        return React.DOM.tr({className: 'item', 'data-selected': this.props.code == this.props.selectedProduct,
+                onClick: this.selectProduct},
+            React.DOM.td(null, this.props.name),
+            React.DOM.td(null, this.props.price),
+            React.DOM.td(null, this.props.url),
+            React.DOM.td(null, this.props.qty),
+            React.DOM.td(null,
+                React.DOM.input({type: 'button', value: 'Delete',
+                    onClick: this.deleteProduct
+                })
+            )
+        )
+    }
+});
---
+++ ishop2/ShopBlock.js
@@ -0,0 +1,55 @@
+﻿var ShopBlock = React.createClass({
+
+    displayName: 'ShopBlock',
+
+    propTypes: {
+        shopName: React.PropTypes.string.isRequired,
+        products: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                name: React.PropTypes.string.isRequired,
+                code: React.PropTypes.number.isRequired,
+                qty: React.PropTypes.number.isRequired,
+                price: React.PropTypes.number.isRequired
+            })
+        ),
+        columns: React.PropTypes.array.isRequired
+    },
+
+    getInitialState: function () {
+        return {
+            products: this.props.products,
+            selectedProduct: null,
+            lastDeletedProduct: null
+        }
+    },
+
+    getSelectedProduct: function (code) {
+        this.setState({selectedProduct: code})
+    },
+
+    deleteProduct: function (code) {
+        var productArr = this.state.products.map((v) => v);
+        this.setState({products: productArr.filter((item) => item.code != code)});
+    },
+    
+    render: function () {
+        var columns = this.props.columns.map((v, i) =>
+            React.DOM.td({key: i}, v)
+        );
+        var products = this.state.products.map((v) =>
+            React.createElement(Product, {
+                key: v.code, name: v.name, price: v.price, qty: v.qty, url: v.url, code: v.code,
+                cbSelectedProduct: this.getSelectedProduct,
+                selectedProduct: this.state.selectedProduct,
+                cbDeleteProduct: this.deleteProduct,
+                deletedProduct: this.state.lastDeletedProduct
+            })
+        );
+        return React.DOM.table({className: 'ShopBlock'},
+            React.DOM.caption(null, this.props.shopName),
+            React.DOM.thead(null,
+                React.DOM.tr(null, columns)),
+            React.DOM.tbody(null, products)
+        );
+    },
+});
